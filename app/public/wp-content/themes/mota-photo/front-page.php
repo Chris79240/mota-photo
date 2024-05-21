bcc32bfd0db99a31664b1f92df66d146e29d3942
Chris79240 <oservice.fr@gmail.com>
2024-05-21 15:26:55 +0200

    single + front-page

---
+++ app/public/wp-content/themes/mota-photo/front-page.php
@@ -0,0 +1,69 @@
+<?php
+get_header();
+
+// Hero section - Récupération d'une image aléatoire pour l'affichage du fond
+$random_images = get_posts(array(
+    'post_type' => 'photo',
+    'posts_per_page' => 1,
+    'orderby' => 'rand',
+    'no_found_rows' => true // Optimise la requête pour ne pas compter les résultats, plus rapide
+));
+
+$hero_image_url = !empty($random_images) ? get_the_post_thumbnail_url($random_images[0]->ID, 'full') : get_stylesheet_directory_uri() . '/assets/images/header.png';
+?>
+
+<div class="hero" style="background-image: url('<?php echo esc_url($hero_image_url); ?>');">
+    <h1>PHOTOGRAPHE EVENT</h1>
+</div>
+
+<div id="photo-filters">
+    <select id="photo-category" class="photo-filter">
+        <option value="">Catégories</option>
+        <?php
+        $categories = get_terms(['taxonomy' => 'categories', 'hide_empty' => false]);
+        foreach ($categories as $category) {
+            echo '<option value="' . esc_attr($category->term_id) . '">' . esc_html($category->name) . '</option>';
+        }
+        ?>
+    </select>
+
+    <select id="photo-format" class="photo-filter">
+        <option value="">Formats</option>
+        <?php
+        $formats = get_terms(['taxonomy' => 'formats', 'hide_empty' => false]);
+        foreach ($formats as $format) {
+            echo '<option value="' . esc_attr($format->term_id) . '">' . esc_html($format->name) . '</option>';
+        }
+        ?>
+    </select>
+
+    <select id="photo-sort" class="photo-filter">
+        <option value="date">Date</option>
+        <option value="title">Titre</option>
+    </select>
+</div>
+
+<div id="photo-gallery">
+    <?php
+    $photo_args = array(
+        'post_type' => 'photo',
+        'posts_per_page' => 8
+    );
+
+    $photo_query = new WP_Query($photo_args);
+
+    if ($photo_query->have_posts()) {
+        while ($photo_query->have_posts()) {
+            $photo_query->the_post();
+            get_template_part('template-parts/photo_block', null, ['id' => get_the_ID()]);
+        }
+    } else {
+        echo "<p>Aucune photo à afficher.</p>";
+    }
+    wp_reset_postdata();
+    ?>
+</div>
+
+<button id="load-more-photos" data-page="1">Charger plus</button>
+
+<?php get_footer(); ?>
